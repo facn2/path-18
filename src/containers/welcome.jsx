9ada1c267e72eeb5d0490b2dc47cf5edd91d5d22
heathercoraje <yhk7830@hotmail.com>
2017-10-10 17:59:12 +0300

    add text and button on welcome page relates#44 add content and button on welcome page which redirects to swiping (/career)

--- src/containers/welcome.jsx
+++ src/containers/welcome.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 const Welcome = () => (
   <div>
-    <h1>Welcome to Path18</h1>
-    <h2>Click button below to find your path </h2>
+    <h1>Path18</h1>
+    <p> Welcome to our app.
+     We aim to show you lots of jobs to help you decide on a suitable career path.
+     We also give you lots of info on the degrees and grades you need. Hope you enjoy it!</p>
+    <button><Link to='/career'>START</Link></button>
   </div>
 );
 
