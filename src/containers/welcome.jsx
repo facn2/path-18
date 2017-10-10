a0568e8e9de91bdee5f3ef2dc2ae5a06e941f97e
heathercoraje <yhk7830@hotmail.com>
2017-10-10 15:54:47 +0300

    create a route for welcome page relates #45

---
+++ src/containers/welcome.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+const Welcome = () => (
+  <div>
+    <h1>Welcome to Path18</h1>
+    <h2>Click button below to find your path </h2>
+  </div>
+);
+
+export default Welcome;
