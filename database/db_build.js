552f0097270fde968e012ff6231e27c7e62a2f6f
matthewdking <matthew.d.king@outlook.com>
2017-10-15 12:27:13 +0300

    remove unnecessary comments relates #82

--- database/db_build.js
+++ database/db_build.js
@@ -1,21 +1,8 @@
-// This file should be run once to import seed data to the database!!
+// This file should be run once to populate the database with seed data!!
 
 const Career = require('./career_model.js');
 const connection = require('./db_connection.js');
 const careerData = require('./seed_data.js');
-
-// db.once('open', () => {
-//   console.log('we are connected to DB');
-//   careerModel.collection.drop(); // once app is open, drop schema career
-//
-//   careerData.forEach((career) => {
-//     career.save((error, result) => {
-//       if (error) {
-//         return console.log(error);
-//       }
-//       return console.log('Saved seed data to database');
-//     });
-//   });
 
 connection.once('open', () => {
   connection.db.dropCollection('careers', (err, res) => {
