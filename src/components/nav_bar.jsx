459b5a534f2fac0573553968194169f28f8ffdff
heathercoraje <yhk7830@hotmail.com>
2017-10-15 12:05:44 +0300

    change name of logoSmall

--- src/components/nav_bar.jsx
+++ src/components/nav_bar.jsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 import List from 'react-icons/lib/fa/list-ul';
 import Help from 'react-icons/lib/fa/question-circle';
 
-const logo = require('../../public/images/logo_small.png');
+const logoSmall = require('../../public/images/logo_small.png');
 
 const NavBar = () => (
   <div>
     <Link to='/list'><List size='2em'/></Link>
-    <Link to='/career'><img size='2em' alt='logo' src={logo} /></Link>
+    <Link to='/career'><img size='2em' alt='logo' src={logoSmall} /></Link>
     <Link to='/help'><Help size='2em'/></Link>
   </div>
 );
