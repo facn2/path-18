7b5b9481c214721953c2ac999a31768542f9b4bd
Vered Rekanati <veredrec@gmail.com>
2017-10-17 14:52:14 +0300

    add sass file to help page Relates #42

--- src/components/help.jsx
+++ src/components/help.jsx
@@ -4,12 +4,13 @@ import NavBar from './../components/nav_bar.jsx';
 const Help = () => (
   <div>
     <NavBar />
-    <h1>How to use the app?</h1>
-    <p>Browse the different jobs</p>
-    <p>Swipe right if you like the job or left if you do not like it</p>
-    <p>You can also click on the LIKE and DISLIKE buttons</p>
-    <p>Click LIST to see the jobs that you like</p>
-    <p>Click HOME to go back to jobs</p>
+    <h1 className='help-question'>How to use the app?</h1>
+    <p className='help-item item-1'>Browse the different jobs</p>
+    <p className='help-item item-2'>
+    Swipe right if you like the job or left if you do not like it</p>
+    <p className='help-item item-3'>You can also click on the LIKE and DISLIKE buttons</p>
+    <p className='help-item item-4'>Click LIST to see the jobs that you like</p>
+    <p className='help-item item-5'>Click HOME to go back to jobs</p>
   </div>
 );
 
