cd1102642b46fbc456ed4f93a8ea2ad964a74242
heathercoraje <yhk7830@hotmail.com>
2017-10-18 12:31:14 +0300

    remove careerImg from store

--- src/store.config.js
+++ src/store.config.js
@@ -3,7 +3,6 @@ import thunk from 'redux-thunk';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { autoRehydrate, persistStore } from 'redux-persist';
 import reducers from './reducers/index';
-import careerImg from './../public/images/example.gif';
 
 
 const store = createStore(
